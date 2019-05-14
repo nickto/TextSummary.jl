9335fac93cd55d03699ba8d6f621c48ca7b3bc88
Nick To <nickto0x64@gmail.com>
2019-05-14 09:56:55 +0200

    Remove TF-IDF from SumBasic

--- src/SumBasic.jl
+++ src/SumBasic.jl
@@ -5,18 +5,12 @@ import ..Sentence
 export score!, score
 
 "Compute scores of each sentence, using SumBasic algorithm."
-function score!(sentences::Array{Sentence,1}, tfidf::Bool=true)
+function score!(sentences::Array{Sentence,1}, tfidf::Bool=false)
     crps = Corpus([s.processed for s in sentences])
     update_lexicon!(crps)
     m = DocumentTermMatrix(crps)
+    m = dtm(m, :dense)
 
-    if tfidf
-        m = tf_idf(m)
-    else
-        m = dtm(m, :dense)
-    end
-
-    m = tf_idf(m)
     for i in 1:size(m, 1)
         sentences[i].weight = sum(m[i, :])
     end
@@ -24,7 +18,7 @@ function score!(sentences::Array{Sentence,1}, tfidf::Bool=true)
 end
 
 "Not in-place version of score."
-function score(sentences::Array{Sentence,1}, tfidf::Bool=true)
+function score(sentences::Array{Sentence,1}, tfidf::Bool=false)
     sentences = deepcopy(sentences)
     score!(sentences, tfidf)
     return sentences
